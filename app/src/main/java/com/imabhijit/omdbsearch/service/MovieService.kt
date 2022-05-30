28b937b4712976c99512c18d9b6b1ba6ef3f9056
imabhijit <abhijit.gupta1122@gmail.com>
2022-05-30 16:56:04 -0400

    create service

---
+++ app/src/main/java/com/imabhijit/omdbsearch/service/MovieService.kt
@@ -0,0 +1,8 @@
+package com.imabhijit.omdbsearch.service
+
+interface MovieService {
+
+    companion object {
+        val API_KEY = "yolo"
+    }
+}
--- app/src/main/java/com/imabhijit/omdbsearch/ui/main/MainViewModel.kt
+++ app/src/main/java/com/imabhijit/omdbsearch/ui/main/MainViewModel.kt
@@ -3,5 +3,4 @@ package com.imabhijit.omdbsearch.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
 }
